a4f8ea4cb194dfd2edd1d11844cbe39a9413e620
F3ValBr <89220737+F3ValBr@users.noreply.github.com>
2023-10-03 00:47:26 -0300

    ADD: Prueba pag perfil
    Estoy probando los enlaces y esas cosas - Felipe

--- apagno_app/apagno_app/urls.py
+++ apagno_app/apagno_app/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from apagno_app.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('testeo/', index),
+    path('perfil_apagno_app/', include('perfil_apagno_app.urls')),
 ]
