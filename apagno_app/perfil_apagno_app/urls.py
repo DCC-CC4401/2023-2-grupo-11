82da78499c06c0d325d8f6ea86f5085628fbb0a5
Carlos Ruz <caruz@dcc.uchile.cl>
2023-10-05 11:45:52 -0300

    ADD: logout path in url.py

--- apagno_app/perfil_apagno_app/urls.py
+++ apagno_app/perfil_apagno_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('testeo2', views.initPerfil),
     path('register', views.register_user, name='register_user'),
-    path('login',views.login_user, name='login'), 
+    path('login',views.login_user, name='login'),
+    path('logout',views.logout_user, name='logout'),
 ]
