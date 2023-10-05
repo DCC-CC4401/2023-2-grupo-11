68fe9541b9ce7eb7e4a58edb179e5b1280a89dbb
gaspargu <gasp.gumucio@gmail.com>
2023-10-05 17:54:14 -0300

    ADD: contact attribute to User class

--- apagno_app/perfil_apagno_app/models.py
+++ apagno_app/perfil_apagno_app/models.py
@@ -12,4 +12,6 @@ class User(AbstractUser):
   pronounsChoices = [('La','La'),('El','El'), ('Le','Le'),('Otro','Otro')]
   pronouns = models.CharField(max_length=5,choices=pronounsChoices)
   nickname = models.CharField(max_length=30)
+  contact = models.CharField(max_length=100)
+
 
