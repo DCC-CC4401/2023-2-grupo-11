8868ef896e75c1be2aa1ba148ae2ad9a449429c3
Carlos Ruz <caruz@dcc.uchile.cl>
2023-10-05 16:26:36 -0300

    ADD: logout url.

--- apagno_app/perfil_apagno_app/urls.py
+++ apagno_app/perfil_apagno_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('testeo2', views.initPerfil),
     path('register', views.register_user, name='register_user'),
     path('login',views.login_user, name='login'),
-    path('logout',views.logout_user, name='logout'),
+    #path('logout',views.logout_user, name='logout'),
 ]
