f0e15305c69e3b16ee0eacadc67931a950f47855
Carlos Ruz <caruz@dcc.uchile.cl>
2023-10-05 11:23:27 -0300

    ADD: login path in url.py

--- apagno_app/perfil_apagno_app/urls.py
+++ apagno_app/perfil_apagno_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('testeo2/', views.initPerfil),
-    path('register', views.register_user, name='register_user'), 
+    path('register', views.register_user, name='register_user'),
+    path('login',views.login_user, name='login'), 
 ]
