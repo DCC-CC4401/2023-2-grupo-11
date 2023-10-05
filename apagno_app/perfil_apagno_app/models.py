3695621b1cb1f12d5ffb80e03f8b8505aaa35490
Carlos Ruz <caruz@dcc.uchile.cl>
2023-10-04 22:29:29 -0300

    ADD: User model.

--- apagno_app/perfil_apagno_app/models.py
+++ apagno_app/perfil_apagno_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+
+
 # Create your models here.
+
+
+# User model:
+# Has pronouns and nickname attributes.
+class User(AbstractUser):
+  pronounsChoices = [('La','La'),('El','El'), ('Le','Le'),('Otro','Otro')]
+  pronouns = models.CharField(max_length=5,choices=pronounsChoices)
+  nickname = models.CharField(max_length=30)
+
+
+  "what"
