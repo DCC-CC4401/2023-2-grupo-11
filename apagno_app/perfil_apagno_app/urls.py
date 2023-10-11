75899412b76b53e713282f060fbca512d30e9ef4
Carlos Ruz <caruz@dcc.uchile.cl>
2023-10-11 19:24:50 -0300

    STYLE: added better name for testeo2 url.

--- apagno_app/perfil_apagno_app/urls.py
+++ apagno_app/perfil_apagno_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('testeo2', views.initPerfil),
+    path('testeo2', views.initPerfil, name='testeo2'),
     path('register', views.register_user, name='register_user'),
     path('login',views.login_user, name='login'),
     path('logout',views.logout_user, name='logout'),
