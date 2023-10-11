f92fb9dfd0264a7817925c3f44686028e7ae224b
Carlos Ruz <caruz@dcc.uchile.cl>
2023-10-11 19:23:31 -0300

    ADD: static urls for loading css and images.

--- apagno_app/apagno_app/settings.py
+++ apagno_app/apagno_app/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'apagno_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'perfil_apagno_app/templates/perfil_apagno_app/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'perfil_apagno_app/templates/perfil_apagno_app/'
+STATICFILES_DIRS = [BASE_DIR / 'perfil_apagno_app/templates/perfil_apagno_app/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
