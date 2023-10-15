8cf5ec0fe9b45772e3594f48c1640deba3218e06
Paooidk <103084858+Paooidk@users.noreply.github.com>
2023-10-15 16:28:35 -0300

    REFACTOR: eliminación de prints para debug

--- apagno_app/apagno_app/views.py
+++ apagno_app/apagno_app/views.py
@@ -4,9 +4,8 @@ from perfil_apagno_app.models import nuevoEvento
 from django.core.paginator import Paginator
 
 def destacados_logged(request):
-    eventos = nuevoEvento.objects.all()  # Obtén todos los eventos desde la base de datos
-    print("Eventos obtenidos:", eventos)  # Agrega esta línea para depuración
-    paginator = Paginator(eventos, 6)
+    eventos = nuevoEvento.objects.all()  #Obtiene todos los eventos desde la base de datos
+    paginator = Paginator(eventos, 6) #Muestra 6 eventos por página
     page = request.GET.get('page')
     eventos = paginator.get_page(page)
     return render(request, "destacados.html", {'eventos': eventos})
