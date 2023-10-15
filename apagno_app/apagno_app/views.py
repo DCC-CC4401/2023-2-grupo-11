1503dcbefbccbe7dc9de406be21d21767638afe3
Paooidk <103084858+Paooidk@users.noreply.github.com>
2023-10-15 16:14:17 -0300

    ADD: implementación de botón 'ver más'

--- apagno_app/apagno_app/views.py
+++ apagno_app/apagno_app/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from perfil_apagno_app.models import nuevoEvento
+from django.core.paginator import Paginator
 
 def destacados_logged(request):
     eventos = nuevoEvento.objects.all()  # Obtén todos los eventos desde la base de datos
-    return render(request, "destacados.html", {'eventos':eventos})
+    print("Eventos obtenidos:", eventos)  # Agrega esta línea para depuración
+    paginator = Paginator(eventos, 6)
+    page = request.GET.get('page')
+    eventos = paginator.get_page(page)
+    return render(request, "destacados.html", {'eventos': eventos})
 
 #def destacados(request):
 #    return render(request, "destacadosSL.html")
