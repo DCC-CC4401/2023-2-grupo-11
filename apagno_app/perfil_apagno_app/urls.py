9299b0c75a9a8fbb2a3d0f6e6c6f983d858f566f
F3ValBr <89220737+F3ValBr@users.noreply.github.com>
2023-11-06 00:53:17 -0300

    ADD: Links para cambio de contraseña, perfil y edicion/eliminacion de eventos

--- apagno_app/perfil_apagno_app/urls.py
+++ apagno_app/perfil_apagno_app/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import MyProfile
 
 from . import views
+
+from django.contrib.auth.views import (
+    LogoutView, 
+    PasswordResetView, 
+    PasswordResetDoneView, 
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 
 urlpatterns = [
     path('crear_evento', views.creacionEvento, name='nuevo evento'), # link al formulario del evento
@@ -10,6 +19,21 @@ urlpatterns = [
     path('register', views.register_user, name='register_user'), # link al formulario de registro
     path('login',views.login_user, name='login'), # link al formulario de inicio de sesion
     path('logout',views.logout_user, name='logout'), # link al formulario de cierre de sesion
+    path('password-reset/', 
+        PasswordResetView.as_view(
+            template_name='users/password_reset.html',
+            html_email_template_name='users/password_reset_email.html'
+        ),
+        name='password-reset'
+    ),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name='password_reset_complete'),
+    path('', views.Eventos.as_view(), name='perfil'),
+    path('evento/<int:pk>', views.EventoDetail.as_view(), name='evento'),
+    path('evento/editar/<int:pk>', views.EventoUpdate.as_view(), name='editar_evento'),
+    path('evento/eliminar/<int:pk>/', views.EventoDelete.as_view(),name='eliminar_evento'),
+    path('profile/', MyProfile.as_view(), name='profile'),
 ]
 
 if settings.DEBUG:
