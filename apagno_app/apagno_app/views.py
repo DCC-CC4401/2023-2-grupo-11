99e9ade74314ca871d3fd9212e628626c2c18424
Paooidk <103084858+Paooidk@users.noreply.github.com>
2023-11-18 18:53:54 -0300

    Paginación de eventos destacados

--- apagno_app/apagno_app/views.py
+++ apagno_app/apagno_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 from perfil_apagno_app.models import nuevoEvento
 from datetime import datetime, time
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def destacados_logged(request):
     # Obtener los parámetros de filtro de la solicitud GET
@@ -14,28 +14,39 @@ def destacados_logged(request):
     ahora = datetime.now() # Se define la fecha y horas actuales
 
     # Obtener todos los eventos desde la base de datos
-    eventos = nuevoEvento.objects.all()
+    eventos_originales = nuevoEvento.objects.all()
 
     # Aplicar los filtros si se han especificado
     if categoria_filtro:
-        eventos = eventos.filter(tipo=categoria_filtro)
+        eventos_originales = eventos_originales.filter(tipo=categoria_filtro)
     if fecha_filtro:
-        eventos = eventos.filter(fecha=fecha_filtro)
+        eventos_originales = eventos_originales.filter(fecha=fecha_filtro)
     if hora_filtro:
         if hora_filtro == "6-14":
-            eventos = eventos.filter(hora__gte=time(6, 0), hora__lt=time(14, 0))
+            eventos_originales = eventos_originales.filter(hora__gte=time(6, 0), hora__lt=time(14, 0))
         elif hora_filtro == "14-20":
-            eventos = eventos.filter(hora__gte=time(14, 0), hora__lt=time(20, 0))
+            eventos_originales = eventos_originales.filter(hora__gte=time(14, 0), hora__lt=time(20, 0))
         elif hora_filtro == "20-6":
-            eventos = eventos.filter(hora__gte=time(20, 0)) | eventos.filter(hora__lt=time(6, 0))
+            eventos_originales = eventos_originales.filter(hora__gte=time(20, 0)) | eventos_originales.filter(hora__lt=time(6, 0))
 
     if estado_filtro == "terminado":
-        eventos = eventos.filter(fecha__lt=ahora.date())
-        eventos = eventos.exclude(fecha=ahora.date(), hora__gt=ahora.time())
+        eventos_originales = eventos_originales.filter(fecha__lt=ahora.date())
+        eventos_originales = eventos_originales.exclude(fecha=ahora.date(), hora__gt=ahora.time())
     elif estado_filtro == "sin_comenzar":
-        eventos = eventos.filter(fecha__gt=ahora.date())
-        eventos = eventos.exclude(fecha=ahora.date(), hora__lte=ahora.time())
-    
+        eventos_originales = eventos_originales.filter(fecha__gt=ahora.date())
+        eventos_originales = eventos_originales.exclude(fecha=ahora.date(), hora__lte=ahora.time())
+
+    # Realizar la paginación de eventos
+    paginator = Paginator(eventos_originales, 4)  # 10 eventos por página
+    page = request.GET.get('page', 1)
+
+    try:
+        eventos = paginator.page(page)
+    except PageNotAnInteger:
+        eventos = paginator.page(1)
+    except EmptyPage:
+        eventos = paginator.page(paginator.num_pages)
+
     if request.method == "GET":
         # Obtener el valor del campo de orden
         orden = request.GET.get('orden')
@@ -43,12 +54,12 @@ def destacados_logged(request):
         # Realiza el procesamiento necesario para ordenar los eventos según la opción seleccionada
         # Aplicar la ordenación
         if orden == 'fecha_asc':
-            eventos = eventos.order_by('fecha')
+            eventos_originales = eventos_originales.order_by('fecha')
         elif orden == 'fecha_desc':
-            eventos = eventos.order_by('-fecha')
+            eventos_originales = eventos_originales.order_by('-fecha')
         else:
-            eventos = eventos.order_by('-fecha')
-    
+            eventos_originales = eventos_originales.order_by('-fecha')
+
     return render(request, "destacados.html", {'eventos': eventos})
 
 
